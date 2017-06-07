aabafbd254fb804bbff1afb77138d76e29078b08
Presentation <Presentation@dannycochran-macbookpro.roam.corp.google.com>
2017-06-07 14:04:57 -0700

    setting up state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {
+  ActivityIndicator,
+  AsyncStorage,
   Text,
   View
 } from 'react-native';
@@ -9,8 +11,68 @@ import styles from './styles';
 import AddInput from './AddInput';
 import AddButton from './AddButton';
 
-export default class App extends React.Component<{}, {}> {
+interface TodoItem {
+  description: string;
+  timestamp: number;
+  id: string;
+  completed: boolean;
+}
+
+interface AppState {
+  todos: TodoItem[];
+  loading: boolean;
+}
+
+export default class App extends React.Component<{}, AppState> {
+  state = {
+    loading: false,
+    todos: []
+  }
+
+  async storeTodos(todos) {
+    await AsyncStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  async fetchTodos() {
+    this.setState({ loading: true });
+    let todos = await AsyncStorage.getItem('todos');
+    todos = JSON.parse(todos) || [];
+
+    this.setState(prevState => ({
+      ...prevState,
+      loading: false,
+      todos
+    }));
+  }
+
+  async addItem(description: string) {
+    const timestamp = Date.now();
+    const todos = [
+      {
+        timestamp,
+        description,
+        id: timestamp + description,
+        completed: false
+      },
+      ...this.state.todos
+    ];
+
+    await this.storeTodos(todos);
+    this.setState(prevState => ({
+      ...prevState,
+      todos
+    }));
+  }
+
+  componentDidMount() {
+    this.fetchTodos();
+  }
+
   render() {
+    if (this.state.loading === true) {
+      return <ActivityIndicator style={styles.centered} size='large' animating={true} />
+    }
+
     return (
       <View style={styles.appContainer}>
         <Text style={styles.headerText}>todos</Text>
