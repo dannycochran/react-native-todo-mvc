f4df3defeb6ecbd77e32144a37302a42245ff439
Presentation <Presentation@dannycochran-macbookpro.roam.corp.google.com>
2017-06-07 09:03:59 -0700

    add button

---
+++ AddButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {
+  Text,
+  TouchableHighlight
+} from 'react-native';
+
+import styles, { buttonUnderlay } from './styles';
+
+export default class AddButton extends React.Component {
+  render() {
+    return (
+      <TouchableHighlight style={styles.addButton}
+        onPress={this.props.addTodo}
+        underlayColor={buttonUnderlay}>
+        <Text style={styles.addText}>Add Todo</Text>
+      </TouchableHighlight>
+    );
+  }
+}
--- index.ios.js
+++ index.ios.js
@@ -5,16 +5,54 @@
  */
 
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, AsyncStorage, StyleSheet, Text, View } from 'react-native';
 
 import styles from './styles';
 
+import AddButton from './AddButton';
 import AddInput from './AddInput';
 
 export default class App extends React.Component {
   state = {
+    todos: [],
     input: '',
+    loading: false,
     editing: false
+  }
+
+  async addTodo() {
+    const timestamp = Date.now();
+    const todos = [
+      {
+        id: timestamp + this.state.input,
+        timestamp,
+        completed: false,
+        description: this.state.input
+      },
+      ...this.state.todos
+    ];
+
+    await this.storeTodos(todos);
+    console.log('new todos:', todos);
+    this.setState({
+      todos,
+      input: ''
+    });
+  }
+
+  async storeTodos(todos) {
+    await AsyncStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  async fetchTodos() {
+    this.setState({ loading: true });
+    let todos = await AsyncStorage.getItem('todos');
+    todos = JSON.parse(todos) || [];
+    this.setState(prevState => ({
+      ...prevState,
+      loading: false,
+      todos
+    }));
   }
 
   onFocusInput(editing) {
@@ -25,11 +63,16 @@ export default class App extends React.Component {
     this.setState({ input });
   }
 
+  componentDidMount() {
+    this.fetchTodos();
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.headerText}>todos</Text>
         <AddInput onFocus={this.onFocusInput.bind(this)} onInput={this.onInput.bind(this)} input={this.state.input}/>
+        {this.state.editing ? <AddButton addTodo={this.addTodo.bind(this)}/> : null }
       </View>
     );
   }
